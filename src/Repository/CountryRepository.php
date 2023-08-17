6fb417c958b09fe406c7c901445eeb3a9f04cad1
Mihai <48624094+mihaichris@users.noreply.github.com>
2023-08-17 21:41:26 +0300

    replace file get contents with httpclientinterface

--- src/Repository/CountryRepository.php
+++ src/Repository/CountryRepository.php
@@ -6,13 +6,19 @@ namespace App\Repository;
 
 use App\Entity\Countries;
 use App\Entity\Country;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class CountryRepository
 {
+    public function __construct(private readonly HttpClientInterface $httpClient)
+    {
+    }
+
     public function getCountries(): Countries
     {
         $countries = new Countries();
-        $countriesData = json_decode(file_get_contents('https://raw.githubusercontent.com/mledoze/countries/master/countries.json'), true);
+        $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/mledoze/countries/master/countries.json');
+        $countriesData = $response->toArray();
         foreach ($countriesData as $countryData) {
             $countries[$countryData['cca3']] =  new Country($countryData['cca3'], $countryData['borders']);
         }
