4a5cc46bcb3742ef12a20cca4f5cf7cfc50669d4
Mihai <48624094+mihaichris@users.noreply.github.com>
2024-01-30 07:53:27 +0200

    Update bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,7 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
